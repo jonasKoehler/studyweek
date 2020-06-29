fb00217155b5db5f7de2a964c381bb5e4f56c6e8
Jonas K <jonaskoehler1997@web.de>
2020-06-29 12:30:26 +0200

    Implemented: Character Movement

---
+++ CovidOperations/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController m_CharacterController;
+    private Vector2 m_Direction;
+    void Start()
+    {
+        m_CharacterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Direction = Vector2.zero;
+        m_Direction.x = Input.GetAxisRaw("Vertical");
+        m_Direction.y = Input.GetAxisRaw("Horizontal");
+        m_CharacterController.Move(m_Direction);
+    }
+}
